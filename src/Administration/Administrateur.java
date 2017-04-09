ee06246ed6b9253d0f7096beb64ecca973083a60
yan-l <gasgano@gmail.com>
2017-04-09 18:46:43 -0400

    patron commande : invoker done

--- src/Administration/Administrateur.java
+++ src/Administration/Administrateur.java
@@ -1,6 +1,7 @@
 package Administration;
 
 import Voyages.*;
+import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class Administrateur implements Observateur{
 	 * @param date
 	 * @param prix
 	 */
-	public void creerItineraire(String cieNom, String idDetail, List<String> lieux, Date hDep, Date hArr, Date date, double prix) {
+	public void creerItineraire(String cieNom, String idDetail, List<String> lieux, Time hDep, Time hArr, Date date, double prix) {
 		// TODO - implement Administrateur.creerItineraire
 		throw new UnsupportedOperationException();
 	}
@@ -76,7 +77,7 @@ public class Administrateur implements Observateur{
 	 * @param date
 	 * @param prix
 	 */
-	public void CreerVoyage(String cieNom, String idDetail, List<String> lieux, Date hDep, Date hArr, Date date, double prix) {
+	public void CreerVoyage(String cieNom, String idDetail, List<String> lieux, Time hDep, Time hArr, Date date, double prix) {
 		// TODO - implement Administrateur.CreerVoyage
 		throw new UnsupportedOperationException();
 	}
@@ -110,7 +111,7 @@ public class Administrateur implements Observateur{
 	 * @param date
 	 * @param prix
 	 */
-	public void ModifierVoyage(Date hdep, Date hArr, Date date, double prix) {
+	public void ModifierVoyage(Time hdep, Time hArr, Date date, double prix) {
 		// TODO - implement Administrateur.ModifierVoyage
 		throw new UnsupportedOperationException();
 	}
